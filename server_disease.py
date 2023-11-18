9de0119a350c0dfe62a43a9ab0df1594046fcf2f
SujalJoshi2003 <109649772+SujalJoshi2003@users.noreply.github.com>
2023-11-19 03:01:50 +0530

    Update server_disease.py

--- server_disease.py
+++ server_disease.py
@@ -46,7 +46,7 @@ def predict_disease(image):
     _, predicted_class = torch.max(output, 1)
     return int(predicted_class)
 
-@app.route('/detect_disease', methods=['POST'])
+@app.route('/detect_disease', methods=['GET'])
 def detect_disease():
     try:
         image = request.files['image']
